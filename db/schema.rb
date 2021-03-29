ba327db05183c87c7288b95914b808abac79ad83
Nyame1997 <ciaravillanueva1997@gmail.com>
2021-03-29 10:51:11 -0400

    Add articles controller, route, models and view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_133746) do
+ActiveRecord::Schema.define(version: 2021_03_29_134622) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
