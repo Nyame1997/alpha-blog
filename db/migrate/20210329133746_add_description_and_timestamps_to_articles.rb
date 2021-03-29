e07640296079f6cace27ec1478b36429d8b9699c
Nyame1997 <ciaravillanueva1997@gmail.com>
2021-03-29 09:54:10 -0400

    Create articles table via migration

---
+++ db/migrate/20210329133746_add_description_and_timestamps_to_articles.rb
@@ -0,0 +1,5 @@
+class AddDescriptionAndTimestampsToArticles < ActiveRecord::Migration[6.1]
+  def change
+  	add_column :articles, :description, :text
+  end
+end
---
+++ db/migrate/20210329134622_change_columns_articles.rb
@@ -0,0 +1,4 @@
+class ChangeColumnsArticles < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
