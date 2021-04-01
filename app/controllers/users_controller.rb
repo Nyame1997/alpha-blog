f8c1d80bf3b65a8dc798001ac2c8acb415eada32
Nyame1997 <ciaravillanueva1997@gmail.com>
2021-03-31 20:25:29 -0400

    Add edit user page and functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,23 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+
+	def edit
+
+		@user = User.find(params[:id])
+
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:notice] = "Account successfully updated!"
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
@@ -11,6 +28,7 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
+
 
 	private
 
