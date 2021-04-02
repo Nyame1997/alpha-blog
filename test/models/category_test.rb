4b2c85cd8a3c2eef25166483c505dcf2e31ca12e
Nyame1997 <ciaravillanueva1997@gmail.com>
2021-04-02 08:57:25 -0400

    Add validation tests for new table categories

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,9 +2,34 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
-	test "Category Validity" do
+	def setup
 		@category = Category.new(name: "Sports")
+	end
+
+	test "Category Validity" do
 		assert @category.valid?
+	end
+
+	test "Name Presence" do
+		@category.name = " "
+		assert_not @category.valid?
+	end
+
+	test "Name Uniqueness" do
+		@category.save
+		@category2 = Category.new(name: "Sports")
+		assert_not @category2.valid?
+	end
+
+	test "Name Length (not too long)" do
+		@category.name = 'a' * 26
+		assert_not @category.valid?
+	end
+
+	test "Name Length (not too short)" do
+		@category.name = 'aa'
+		assert_not @category.valid?
+
 	end
 
 end 
