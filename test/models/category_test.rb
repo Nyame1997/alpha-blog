25b5636ff4ad1da1b75067b4332c226e3bbeac90
Nyame1997 <ciaravillanueva1997@gmail.com>
2021-04-02 10:07:58 -0400

    Add integration test for create categories

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -6,30 +6,29 @@ class CategoryTest < ActiveSupport::TestCase
 		@category = Category.new(name: "Sports")
 	end
 
-	test "Category Validity" do
+	test "name should be valid" do
 		assert @category.valid?
 	end
 
-	test "Name Presence" do
+	test "name should be present" do
 		@category.name = " "
 		assert_not @category.valid?
 	end
 
-	test "Name Uniqueness" do
+	test "name should be unique" do
 		@category.save
 		@category2 = Category.new(name: "Sports")
 		assert_not @category2.valid?
 	end
 
-	test "Name Length (not too long)" do
+	test "name should not be too long" do
 		@category.name = 'a' * 26
 		assert_not @category.valid?
 	end
 
-	test "Name Length (not too short)" do
+	test "name should not be too short" do
 		@category.name = 'aa'
 		assert_not @category.valid?
-
 	end
 
 end 
